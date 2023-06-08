a21e91f6f0d16defd559d2177c47b327471c5f3d
Damian Cichosz <damekjodamek@gmail.com>
2023-06-09 00:58:29 +0200

    Komentarze, imgur, troche wyglądu

--- src/components/Stopka.jsx
+++ src/components/Stopka.jsx
@@ -7,6 +7,8 @@ export default function Menu(){
     <footer>
         <p className='DC4P'>Damian Cichosz 4P</p>
         <a href="https://www.youtube.com/watch?v=mbD-cYMDlCg" target='_blank' className='rick'>YouTube</a>
+        <br></br>
+        <a href="https://imgur.com/a/BQ1C7M0" target='_blank' className='mem'>Imgur</a>
     </footer>
   );
 }
--- src/pages/Szukaj.js
+++ src/pages/Szukaj.js
@@ -5,6 +5,7 @@ function Szukaj() {
   const reviews = JSON.parse(localStorage.getItem('reviews')) || [];
   const [searchKeyword, setSearchKeyword] = useState('');
   const [likedReviews, setLikedReviews] = useState([]);
+  const [comments, setComments] = useState([]);
 
   const handleSearch = (e) => {
     setSearchKeyword(e.target.value);
@@ -14,6 +15,20 @@ function Szukaj() {
     const updatedLikedReviews = [...likedReviews];
     updatedLikedReviews[index] = !likedReviews[index];
     setLikedReviews(updatedLikedReviews);
+  };
+
+  const handleCommentChange = (e, index) => {
+    const updatedComments = [...comments];
+    updatedComments[index] = e.target.value;
+    setComments(updatedComments);
+  };
+
+  const handleCommentSubmit = (index) => {
+    const updatedReviews = [...reviews];
+    updatedReviews[index].comments = updatedReviews[index].comments || [];
+    updatedReviews[index].comments.push(comments[index]);
+    localStorage.setItem('reviews', JSON.stringify(updatedReviews));
+    setComments([]);
   };
 
   const filteredReviews = reviews.filter((review) => {
@@ -27,11 +42,28 @@ function Szukaj() {
         {filteredReviews.map((review, index) => (
           <li key={index}>
             <h3>{review.title}</h3>
-            <p>Ocena: {review.rating}/10</p>
-            <p>Opinia: {review.review}</p>
-            <button onClick={() => handleLike(index)}>
+            <p><b>Ocena:</b> {review.rating}/10</p>
+            <p><b>Opinia:</b> {review.review}</p>
+            <button onClick={() => handleLike(index)} className='like'>
               {likedReviews[index] ? 'Polubione' : 'Polub'}
             </button>
+            <br></br>
+            <br></br>
+            <h3>Komentarze</h3>
+            <div>
+              <input type="text" value={comments[index] || ''} onChange={(e) => handleCommentChange(e, index)} placeholder="Dodaj komentarz..." />
+              <br></br>
+              <button onClick={() => handleCommentSubmit(index)} className='add'>Dodaj</button>
+           
+            </div>
+            <br></br>
+            {review.comments && (
+              <ul>
+                {review.comments.map((comment, commentIndex) => (
+                  <li key={commentIndex}>{comment}</li>
+                ))}
+              </ul>
+            )}
             <hr />
           </li>
         ))}
